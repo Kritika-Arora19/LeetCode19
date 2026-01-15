62d6d22a5ebe3680d1c592e1bed22de7b6ebfbb8
Kritika Arora <kritikarora190809@gmail.com>
2026-01-15 21:42:59 +0530

    Time: 0 ms (100%), Space: 43.6 MB (14.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1;i>=0;i--)
-        {
-            if(digits[i]==9){
-            digits[i]=0;
-            }
-            else{
-                digits[i]++;
-                return digits;
-            }
+        int n = digits.length;
+    for (int i = n - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
         }
-        digits= new int[digits.length+1];
-        digits[0]=1;
-        return digits;
+        digits[i] = 0;
     }
+    int[] result = new int[n + 1];
+    result[0] = 1;
+    return result;
 }
+    }

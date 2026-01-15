804531152ef3a75d640af0f218a2bcd5f46f8b51
Kritika Arora <kritikarora190809@gmail.com>
2026-01-15 15:03:56 +0530

    Time: 1 ms (96.13%), Space: 47.4 MB (7.38%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -4,8 +4,7 @@ class Solution {
         int[] ans = new int[n*2];
         for(int i=0;i<n;i++)
         {
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
+            ans[i]=ans[i+n]=nums[i];
         }
         return ans;
     }

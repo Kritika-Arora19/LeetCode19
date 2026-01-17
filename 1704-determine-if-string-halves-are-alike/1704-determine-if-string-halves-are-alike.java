ba67754016749ea48487699c42783fdc2350aae4
Kritika Arora <kritikarora190809@gmail.com>
2026-01-17 14:58:25 +0530

    Time: 1 ms (99.92%), Space: 42.9 MB (89.18%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int n=s.length();
-        int count=0,count1=0;
-        char c,c1;
-        String s1=s.substring(0,n/2);
-        String s2=s.substring(n/2,n);
-        for(int i=0;i<s1.length();i++)
-        {
-            c=s1.charAt(i);
-            if("aeiouAEIOU".indexOf(c) !=-1)
-            count++;
+          return vowelCount(s.substring(0, s.length()/2)) == vowelCount(s.substring(s.length()/2, s.length()));
+    }
+
+    public int vowelCount(String s) {
+        int count=0;
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch =='U') count++;
         }
-        for(int j=0;j<s2.length();j++)
-        {
-            c1=s2.charAt(j);
-            if("aeiouAEIOU".indexOf(c1) !=-1)
-            count1++;
-        }
-        if(count==count1)
-        return true;
-        else
-        return false;
+        return count;
     }
 }

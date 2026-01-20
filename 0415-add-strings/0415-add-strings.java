ca230b21a1b46aa3b3b5343ef47b3f6a88c3714a
Kritika Arora <kritikarora190809@gmail.com>
2026-01-20 17:29:09 +0530

    Time: 2 ms (97.34%), Space: 43.9 MB (69.6%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,11 +1,18 @@
-import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-           BigInteger a = new BigInteger(num1);
-        BigInteger b = new BigInteger(num2);
+         int i = num1.length()-1;
+      int j = num2.length()-1;
+      int c=0;
 
-        BigInteger ans = a.add(b);
-        String num3 = String.valueOf(ans);
-        return num3;
+      StringBuilder sb = new StringBuilder();
+      while(i>=0 || j>=0 || c!=0){
+        int d1 = (i>=0) ? num1.charAt(i--)-'0' : 0;
+        int d2 = (j>=0) ? num2.charAt(j--)-'0' : 0;
+        int sum = d1+d2+c;
+        sb.append(sum%10);
+        c = sum/10;
+      }
+
+      return sb.reverse().toString();
     }
 }

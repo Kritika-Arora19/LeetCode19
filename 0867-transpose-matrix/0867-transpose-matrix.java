ed738fc7f26c4108287496aaef1b6f51834315c0
Kritika Arora <kritikarora190809@gmail.com>
2026-01-21 14:53:58 +0530

    Time: 0 ms (100%), Space: 47 MB (17.32%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -3,11 +3,9 @@ class Solution {
         int r=matrix.length;
         int c=matrix[0].length;
         int[][] newm= new int[c][r];
-        int c1=r;
-        int r1=c;
-        for(int i=0;i<r1;i++)
+        for(int i=0;i<c;i++)
         {
-            for(int j=0;j<c1;j++)
+            for(int j=0;j<r;j++)
             {
                 newm[i][j]=matrix[j][i];
             }

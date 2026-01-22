d132efd05738eca790c088a52816ec198a7ad418
Kritika Arora <kritikarora190809@gmail.com>
2026-01-22 20:33:23 +0530

    Time: 5 ms (6.29%), Space: 43.5 MB (58.17%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,8 +1,7 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
         int n = logs.length;
-        int ans = 0;
-        int max = 0; 
+        int ans = 0,max=0; 
         for (int i = 0; i < n; i++) {
             int year = logs[i][0];
             int count = 0; 

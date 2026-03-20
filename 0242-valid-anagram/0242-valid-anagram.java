086e4be653b6af5b2ec58c86e8f87f55e2a481e1
Kritika Arora <kritikarora190809@gmail.com>
2026-03-20 20:36:03 +0530

    Time: 4 ms (81.55%), Space: 44 MB (98.48%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        return false;
+
+        int []a = new int[26];
+        int []b=new int[26];
+        for(int h=0;h<26;h++)
+        {
+            a[h]=0;
+            b[h]=0;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            int c1=(int)c-97;
+            a[c1]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char c=t.charAt(i);
+            int c2=(int)c-97;
+            b[c2]++;
+        }
+        for(int j=0;j<26;j++)
+        {
+            if(a[j]!=b[j])
+            return false;
+        }
+        return true;
+    }
+}
